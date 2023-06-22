1ae54504b19b9cb552556a90b3418fb3cbb1bffa
Cherry <sarahandecherry@gmail.com>
2023-06-22 14:24:41 -0700

    started work on home page, nothing yet on not found

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,26 @@
-import React from 'react' 
-
+import React from "react"
+//add background to assets folder and fix this file path
+import background from "./assets/"
 
 const Home = () => {
-    return (
+    return(
         <>
-            <h1>This is the Homepage</h1>
+            <div style={{ backgroundImage: 'url(${background})', height: "100vh", width: "100vw"}}>
+                <h1>Travelogue</h1>
+                <h4>Track your globetrotting adventures as you trek across the world, one destination at a time</h4>
+            </div>
         </>
-
-    );
+    )
 }
+export default Home
 
-export default Home;
+//One of the following should work for fonts
+// For App.css - 
+// @font-face{
+//     font-family: 'Pathway Gothic One';
+//     src: url(http://fonts.google.com/specimen/Pathway+Gothic+One);
+//     font-weight: bold;
+//     font-style: normal;
+// }
+
+// @import url('https://fonts.googleapis.com/css2?family=Pathway+Gothic+One:wght@300,400,500,600,700,800,900&display=swap');
