5e233f938bcc69473a3565d3e78ce6fabdc06c33
Romoramel <romoramel@hotmail.com>
2023-06-22 17:17:50 -0700

    basic components for about us page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,13 +1,169 @@
 import React from 'react' 
 
 
-const AboutUs = () => {
-    return (
-        <>
-            <h1>This is the About Us Page</h1>
-        </>
+// const AboutUs = () => {
+//     return (
+//         <>
+//             <h1>This is the About Us Page</h1>
+//         </>
 
-    );
+//     );
+// }
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
 }
 
+html {
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+.column {
+  float: left;
+  width: 33.3%;
+  margin-bottom: 16px;
+  padding: 0 8px;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  margin: 8px;
+}
+
+.about-section {
+  padding: 50px;
+  text-align: center;
+  background-color: #474e5d;
+  color: white;
+}
+
+.container {
+  padding: 0 16px;
+}
+
+.container::after, .row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.title {
+  color: grey;
+}
+
+.button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+}
+
+.button:hover {
+  background-color: #555;
+}
+
+@media screen and (max-width: 650px) {
+  .column {
+    width: 100%;
+    display: block;
+  }
+}
+</style>
+</head>
+<body>
+
+<div class="about-section">
+  <h1>Meet The Team</h1>
+  <p>Some text about who we are and what we do.</p>
+  <p>Resize the browser window to see that this page is responsive by the way.</p>
+</div>
+
+<h2 style="text-align:center">Our Team</h2>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="/w3images/team1.jpg" alt="Jane" style="width:100%">
+      <div class="container">
+        <h2>Sarah Cherry</h2>
+        <p class="title">Tech Lead/Anchor</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <img className="img-responsive" src="public/assets/image/img/footer-logo.jpg" alt="logo"/>
+        <img className="img-responsive" src="public/assets/image/img/footer-logo.jpg" alt="logo"/>
+
+
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="/w3images/team2.jpg" alt="Mike" style="width:100%">
+      <div class="container">
+        <h2>Scott Tuschl</h2>
+        <p class="title">Product Manager</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <img className="img-responsive" src="public/assets/image/img/footer-logo.jpg" alt="logo"/>
+
+        <img className="img-responsive" src="public/assets/image/img/footer-logo.jpg" alt="logo"/>
+
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="column">
+    <div class="card">
+      <img src="/w3images/team3.jpg" alt="John" style="width:100%">
+      <div class="container">
+        <h2>Surielis Rodriguez</h2>
+        <p class="title">Lead Design</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <img className="img-responsive" src="public/assets/image/img/footer-logo.jpg" alt="logo"/>
+
+       <img className="img-responsive" src="public/assets/image/img/footer-logo.jpg" alt="logo"/>
+
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="column">
+    <div class="card">
+      <img src="/w3images/team3.jpg" alt="John" style="width:100%">
+      <div class="container">
+        <h2>Ramgel Romo</h2>
+        <p class="title">Project Manager</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <img className="img-responsive" src="public/assets/image/img/footer-logo.jpg" alt="logo"/>
+
+       <img className="img-responsive" src="public/assets/image/img/footer-logo.jpg" alt="logo"/>
+
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
+
+
 export default AboutUs;
+
