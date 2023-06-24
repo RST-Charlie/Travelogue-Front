a1b8d35cce59799c448724bb3ba6174c8be9800d
scott198989 <youremail@example.comscott-tuschl@comcast.net>
2023-06-24 17:22:48 -0500

    edit page comeplted, test has passed --ST

--- src/__tests__/TravelLogueEdit.test.js
+++ src/__tests__/TravelLogueEdit.test.js
@@ -1,19 +1,18 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import TravelLogueEdit from '../pages/TravelLogueEdit';
 
-describe('<TravelLogueEdit />', () => {
-  beforeEach(() => {
-    render(
-      <BrowserRouter>
-        <TravelLogueEdit />
-      </BrowserRouter>
-    );
-  });
+describe("<TravelLogueEdit />", () => {
+    beforeEach(() => {
+        render(
+            <BrowserRouter>
+                <TravelLogueEdit />
+            </BrowserRouter>
+        );
+    });
 
-  it('has an entry input', () => {
-    const entryInput = screen.getByLabelText('Entry');
-    expect(entryInput).toBeInTheDocument();
-  });
+    it("renders the travellogue edit page", () => {
+        const element = screen.getByRole('button', { name: /submit updated trip/i });
+        expect(element).toBeInTheDocument();
+    });
 });
