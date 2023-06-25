ec64a22f950cd5bb02d08a440b7d8422dca28eb0
scott198989 <youremail@example.comscott-tuschl@comcast.net>
2023-06-24 21:36:22 -0500

    protected index scaffolding working, waiting on index --ST

--- src/pages/TravelLogueProtectedIndex.js
+++ src/pages/TravelLogueProtectedIndex.js
@@ -1,13 +1,42 @@
-import React from 'react' 
+import React from 'react';
+import { Card, CardGroup, CardImg, CardBody, CardTitle, Button, NavLink } from "reactstrap"
 
-
-const TravelLogueProtectedIndex = () => {
+const TravelLogueProtectedIndex = ({ currentUser, trips }) => {
+    const myTrips = trips?.filter(trip => currentUser?.id === trip.user_id)
     return (
         <>
-            <h1>This is the Protected Index Page</h1>
+            <CardGroup>
+                {myTrips?.map((trip, index) => {
+                    return (
+                        <Card key={index}>
+                            <CardImg
+                                alt="Fun in the sun!!/"
+                                src={trip.image}
+                                top
+                                width="100%"
+                            />
+                            <CardBody>
+                                <CardTitle tag="h5">
+                                    Located in {trip.country}
+                                </CardTitle>
+                                <Button>
+                                    <NavLink href={`/TravelLogueShow/${trip.id}`}>
+                                        More Details
+                                    </NavLink>
+                                </Button>
+                                <Button>
+                                    Edit
+                                </Button>
+                                <Button>
+                                    Delete
+                                </Button>
+                            </CardBody>
+                        </Card>
+                    )
+                })}
+            </CardGroup>
         </>
-
-    );
+    )
 }
 
 export default TravelLogueProtectedIndex;
