4b6f0d3457d4686f5f7e7e7e8ff8f57ecc87bfff
surielisrodriguez <surielisrodriguez7@gmail.com>
2023-06-26 16:20:08 -0700

    created sign up and log in styling with testing

--- src/__tests__/SignUp.test.js
+++ src/__tests__/SignUp.test.js
@@ -1,19 +1,50 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import SignUp from '../pages/SignUp';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import SignUp from "../pages/SignUp";
 
-describe('<SignUp />', () => {
-    beforeEach(() => {
+describe("SignUp", () => {
+    it("renders the SignUp component", () => {
         render(
-            <BrowserRouter>
+            <MemoryRouter>
                 <SignUp />
-            </BrowserRouter>
+            </MemoryRouter>
         );
+        expect(screen.getByTestId("signup-component")).toBeInTheDocument();
     });
 
-    it('has an email input', () => {
-        const emailInput = screen.getByPlaceholderText('email');
-        expect(emailInput).toBeInTheDocument();
+    it("renders the form inputs", () => {
+        render(
+            <MemoryRouter>
+                <SignUp />
+            </MemoryRouter>
+        );
+        expect(screen.getByPlaceholderText("Email")).toBeInTheDocument();
+        expect(screen.getByPlaceholderText("Password")).toBeInTheDocument();
+        expect(screen.getByPlaceholderText("Confirm Password")).toBeInTheDocument();
+        expect(screen.getByRole("button", { name: "Submit" })).toBeInTheDocument();
+    });
+
+    it("submits the form", () => {
+        render(
+            <MemoryRouter>
+                <SignUp />
+            </MemoryRouter>
+        );
+        fireEvent.submit(screen.getByRole("button", { name: "Submit" }));
+    });
+
+    it("navigates to the login page", () => {
+        render(
+            <MemoryRouter>
+                <SignUp />
+            </MemoryRouter>
+        );
+        fireEvent.click(screen.getByText("Login"));
     });
 });
+
+
+
+
+
