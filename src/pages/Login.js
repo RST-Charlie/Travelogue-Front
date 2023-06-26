017925b300ebf96d271db4f413ed5ec8c5aa8254
scott198989 <youremail@example.comscott-tuschl@comcast.net>
2023-06-25 19:23:09 -0500

    thought I pushed from last night, pushing again --ST

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,58 +1,61 @@
 import React, { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../Styles/Login.css';
 
 const Login = ({ login }) => {
-    const formRef = useRef();
-    const navigate = useNavigate();
-    const [loggedIn, setLoggedIn] = useState(false);
+  const formRef = useRef();
+  const navigate = useNavigate();
+  const [loggedIn, setLoggedIn] = useState(false);
 
-    const handleLogin = () => {
+  const handleLogin = () => {
+    setLoggedIn(true);
+    navigate('/');
+  };
 
-        setLoggedIn(true);
-        navigate('/');
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(formRef.current);
+    const data = Object.fromEntries(formData);
+    const userInfo = {
+      user: { email: data.email, password: data.password },
     };
+    console.log(userInfo);
+    handleLogin();
+    e.target.reset();
+  };
 
-    const handleLogout = () => {
-
-        setLoggedIn(false);
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const formData = new FormData(formRef.current);
-        const data = Object.fromEntries(formData);
-        const userInfo = {
-            user: { email: data.email, password: data.password },
-        };
-        console.log(userInfo);
-        handleLogin();
-        e.target.reset();
-    };
-
-    return (
-        <div id="login">
-            <h1>Sign In</h1>
-            {!loggedIn ? (
-                <form ref={formRef} onSubmit={handleSubmit}>
-                    Email: <input type="email" name="email" placeholder="email" />
-                    <br />
-                    Password: <input type="password" name="password" placeholder="password" />
-                    <br />
-                    <input type="submit" value="Login" />
-                </form>
-            ) : (
-                <div>
-                    <p>You are logged in!</p>
-                    <button onClick={handleLogout}>Logout</button>
-                </div>
-            )}
-            <br />
-            <div>
-                Not registered yet, <a href="/signup">Signup</a>
-            </div>
-        </div>
-    );
+  return (
+    <div className="wrapper">
+      <div className="form-wrapper sign-in">
+        <form onSubmit={handleSubmit} ref={formRef}>
+          <h2>Log In</h2>
+          <div className="input-group">
+            <label htmlFor="email">Username</label>
+            <input type="text" name="email" required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" required />
+          </div>
+          <div className="remember">
+            <label htmlFor="remember">
+              <input type="checkbox" id="remember" /> Remember Me
+            </label>
+          </div>
+          <button type="submit">Log In</button>
+          <div className="signup-link">
+            <p>
+              Don't have an account yet? <a href="#" className="signup-link">Sign Up</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
-
