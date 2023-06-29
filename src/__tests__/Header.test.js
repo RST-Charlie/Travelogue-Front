f4dfe2ccfeb84baacc10fab70b1af2d4ddfbd333
surielisrodriguez <surielisrodriguez7@gmail.com>
2023-06-29 14:26:25 -0700

    Fixed test to pass

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import Header from '../components/Header';
 
 describe('Header', () => {
-    it('renders the navbar with the correct text', () => {
-        const { getByText } = render(<Header />);
-        const homeLink = getByText(/RSTech/i);
-        expect(homeLink).toBeInTheDocument();
-    });
+  it('renders the navbar with the correct text', () => {
+    const { getByText } = render(
+      <BrowserRouter> 
+        <Header />
+      </BrowserRouter>
+    );
+    const homeLink = getByText(/RSTech/i);
+    expect(homeLink).toBeInTheDocument();
+  });
 });
