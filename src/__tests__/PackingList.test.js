7e608d9b6d1fd302a5fce9d06ec31b9d3d7d12bf
scott198989 <youremail@example.comscott-tuschl@comcast.net>
2023-06-30 11:49:42 -0500

    spacing fix

--- src/__tests__/PackingList.test.js
+++ src/__tests__/PackingList.test.js
@@ -4,22 +4,22 @@ import { BrowserRouter } from 'react-router-dom';
 import PackingList from '../pages/PackingList';
 
 describe('<PackingList />', () => {
-  beforeEach(() => {
-    render(
-      <BrowserRouter>
-        <PackingList />
-      </BrowserRouter>
-    );
-  });
+    beforeEach(() => {
+        render(
+            <BrowserRouter>
+                <PackingList />
+            </BrowserRouter>
+        );
+    });
 
-  it('allows a user to input information into the form', () => {
-    const input = screen.getByPlaceholderText(/Add an item/i);
-    fireEvent.change(input, { target: { value: 'Test input' } });
-    expect(input.value).toBe('Test input');
-  });
+    it('allows a user to input information into the form', () => {
+        const input = screen.getByPlaceholderText(/Add an item/i);
+        fireEvent.change(input, { target: { value: 'Test input' } });
+        expect(input.value).toBe('Test input');
+    });
 
-  it('renders "Add" on the button', () => {
-    const addButton = screen.getByText(/Add/i);
-    expect(addButton).toBeInTheDocument();
-  });
+    it('renders "Add" on the button', () => {
+        const addButton = screen.getByText(/Add/i);
+        expect(addButton).toBeInTheDocument();
+    });
 });
