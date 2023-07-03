fe1720692a675648118f80d3e491573a23b19c53
Cherry <sarahandecherry@gmail.com>
2023-07-03 13:32:09 -0700

    remove unused mockUsers, unused imports

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import "../styles/Home.css"
 import Plane from "../assets/planebanner.png"
-import Globe from "../assets/logo-globe.png"
-import Logo from "../assets/Travelogo.png"
 import 'animate.css';
 
 const Home = () => {
